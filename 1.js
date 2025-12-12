087213102ce597fc4ff86d112e5da5848a3abca5
pabloVICH-hehe <pablosnusvich@gmail.com>
2025-12-12 16:02:06 +0700

    Create 1.js

---
+++ 1.js
@@ -0,0 +1,12 @@
+const mod1 = document.getElementById('mod1');
+const mod2 = document.getElementById('mod2');
+const mod3 = document.getElementById('mod3');
+mod1.addEventListener('click', (event) => {
+    window.open('https://fonts.googleapis.com', '_self');
+});
+mod2.addEventListener('click', (event) => {
+    window.open('https://mari-shipit.github.io/', '_self');
+});
+mod3.addEventListener('click', (event) => {
+    window.open('#', '_self');
+});
